d568e33104217360c48af873adbf9f304cce0c63
nopphasinara <nopphasin.arayasirikul@gmail.com>
2017-12-21 17:04:17 +0700

    upload

--- lib/laravel.sh
+++ lib/laravel.sh
@@ -7,7 +7,14 @@ artisan() {
 laravel-project() {
   composer create-project laravel/laravel $*
 
-  (cd $1 && chmod -R storage bootstrap/cache database/seeds && composer install && composer require laravelnews/laravel-twbs4 && php artisan preset bootstrap4-auth && npm install && npm run dev)
+  cd $1 && chmod -R storage bootstrap/cache database/seeds && composer install && cd ../
+}
+
+laravel-frontend-preset() {
+  composer require laravelnews/laravel-twbs4
+  php artisan preset bootstrap4-auth
+  npm install
+  npm run dev
 }
 
 # init() {
