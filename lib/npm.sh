3f9b2e58d405bcb002c4cc19880f0dc367f987ae
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-01-09 16:03:19 +0700

    upload

--- lib/npm.sh
+++ lib/npm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Installed: tern
+# Installed: tern angular @angular/router @angular/core @angular/compiler @angular/animations @angular/cli angular-ui-bootstrap @angular/cdk @angular/material protractor @ngtools/webpack@angular-devkit/build-optimizer @angular/flex-layout gulp-angular-templatecache
 
 npml() {
   re='^[0-9]+$'
 
   global=''
-  depth=''
+  depth='--depth=0'
   while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
     case $1 in
       -d | --depth)
