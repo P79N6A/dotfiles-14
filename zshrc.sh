7d73a99ce3dd1b44b0f81ffbc90e9c5394693a90
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-02-28 09:35:02 +0700

    upload

--- zshrc.sh
+++ zshrc.sh
@@ -1,6 +1,8 @@
 export DOTFILES="${HOME}/.dotfiles"
 export ZSHFILES="${DOTFILES}/zsh"
 
+source $ZSHFILES/exports.sh
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
@@ -10,7 +12,12 @@ export ZSH=$ZSHFILES
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+# ZSH_THEME="amuse"
+# ZSH_THEME="cypher"
+# ZSH_THEME="dpoggi"
+# ZSH_THEME="kolo"
+# ZSH_THEME="nanotech"
+ZSH_THEME="theunraveler"
 
 # Set list of themes to load
 # Setting this variable when ZSH_THEME=random
@@ -20,14 +27,14 @@ ZSH_THEME="robbyrussell"
 # ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
 
 # Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
+CASE_SENSITIVE="false"
 
 # Uncomment the following line to use hyphen-insensitive completion. Case
 # sensitive completion must be off. _ and - will be interchangeable.
 # HYPHEN_INSENSITIVE="true"
 
 # Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
+DISABLE_AUTO_UPDATE="true"
 
 # Uncomment the following line to change how often to auto-update (in days).
 # export UPDATE_ZSH_DAYS=13
@@ -39,20 +46,20 @@ ZSH_THEME="robbyrussell"
 # DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+ENABLE_CORRECTION="false"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
+COMPLETION_WAITING_DOTS="true"
 
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
+DISABLE_UNTRACKED_FILES_DIRTY="true"
 
 # Uncomment the following line if you want to change the command execution time
 # stamp shown in the history command output.
 # The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# HIST_STAMPS="mm/dd/yyyy"
+HIST_STAMPS="mm dd, yyyy"
 
 # Would you like to use another custom folder than $ZSH/custom?
 # ZSH_CUSTOM=/path/to/new-custom-folder
@@ -80,6 +87,5 @@ source $ZSH/oh-my-zsh.sh
 
 ## LOAD SOURCES
 ## -------------------------------------------------- ##
-source $ZSHFILES/exports.sh
 source $ZSHFILES/functions.sh
 source $ZSHFILES/alias.sh
