6949c90e9a410843345b8b14608702d27d471f81
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-03-06 19:53:04 +0700

    Update source

--- zsh/lib/git.zsh
+++ zsh/lib/git.zsh
@@ -4,8 +4,8 @@ function git_prompt_info() {
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-    # echo "${ref#refs/heads/}$(parse_git_dirty)"
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$(git_prompt_status)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    echo "$(git_current_branch):$(parse_git_dirty):$(git_prompt_status):$(git_remote_status)"
+    # echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$(git_prompt_status)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
 
