96b395ce6e40dc1d0a2e3bdbf3cef59c7f9cf876
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-04-09 02:00:50 +0700

    Update source

---
+++ projects/sss/sss/sss.m
@@ -0,0 +1,20 @@
+//
+//  sss.m
+//  sss
+//
+//  Created by Sudprawat Atae on 9/4/18.
+//  Copyright © 2018 Sudprawat Atae. All rights reserved.
+//
+
+#import "sss.h"
+
+@implementation sss
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo
+{
+    // Add your code here, returning the data to be passed to the next action.
+    
+    return input;
+}
+
+@end
