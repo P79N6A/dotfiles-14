712a41159a26e3d092bf39ff35fdc4e5fcfc95bb
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-04-22 07:58:28 +0700

    Initial commit

---
+++ bin/gallery-functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function downloadCatalog($url) {
+    exec('curl -o ./'. basename($url) .' --url '. $url .'', $stdout, $status);
+    return $status;
+}
+
+function createDataLog($source, $file) {
+    $data = [];
+
+    if (file_exists($file)) {
+        @unlink($file);
+    }
+
+    shell_exec('touch '. $file .' && chmod -R 0777 '. $file .'');
+
+    echo "\nWriting data to file... {$file}\n";
+
+    $fh = @fopen($source, 'r');
+    while (!feof($fh)) {
+        $buffer = trim(@fgets($fh));
+        if ($buffer) {
+            $buffer = explode('::', $buffer);
+            $data[] = explode('::', $buffer);
+            $fw = @fopen($file, 'a+');
+            fwrite($fw, "". trim($buffer[2]) ."\n");
+            @fclose($fw);
+        }
+
+        @ob_flush();
+        usleep(400);
+    }
+    @fclose($fh);
+
+    echo "\nDownloading... {$file}\n";
+    foreach ($data as $index => $value) {
+        $file = trim($value[2]);
+        downloadFile($file, basename($file));
+    }
+}
+
+function downloadFile($file, $output) {
+    shell_exec('curl -o '. $file .' '. $output .'');
+}
