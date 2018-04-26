1eab07cf5626b536dcdc7d38ef0eec0cb29b5377
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-04-27 01:37:38 +0700

    Update source

--- zsh/lib/git.zsh
+++ zsh/lib/git.zsh
@@ -9,7 +9,7 @@ function git_prompt_info() {
     if [[ `git_commits_ahead` == 1 ]]; then
       ZSH_THEME_GIT_PROMPT_CLEAN=''
     fi
-    echo "%F{green}%f %F{yellow}%100c%f $(git_current_branch)$(parse_git_dirty)$(git_prompt_status) [$(git_prompt_short_sha)]%F{blue}[%f %F{green}%D{%L:%M}%f %F{yellow}%D{%p}%f %F{blue}]%f
+    echo "%F{green}%f %F{yellow}%100c%f $(git_current_branch)$(parse_git_dirty)$(git_prompt_status) %F{blue}[%f %F{green}%D{%L:%M}%f %F{yellow}%D{%p}%f %F{blue}]%f
 %F{white}$%f "
     # echo "$(git_current_branch)$(parse_git_dirty)$(git_prompt_status)"
   fi
