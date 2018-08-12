6b70e7671ff96a60a643a37a2033f156ecb59091
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-08-12 15:33:20 +0700

    Update source

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -171,7 +171,7 @@ function pullall() {
 }
 
 function pushall() {
-  echo -ne "${YELLOW}Pushing... .dotfiles${NORMAL}\n";
+  printf "${YELLOW}Pushing... .dotfiles${NORMAL}\n";
   dotf; add; commit; push;
   echo -ne "Pushing... .atom\n";
   atm; add; commit; push;
