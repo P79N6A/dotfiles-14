5227c5cdce7c98abaae80448bbe6ec5b06bf9bb1
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-01-31 19:45:59 +0700

    Update source

---
+++ oh-my-zsh/custom/plugins/time/time.plugin.zsh
@@ -0,0 +1,66 @@
+typeset -g geometry_time_human
+
+-prompt_geometry_time_long_format() {
+  local human=""
+  local days=$1
+  local hours=$2
+  local minutes=$3
+  local seconds=$4
+
+  (( days > 0 )) && human+="${days}d "
+  (( hours > 0 )) && human+="${hours}h "
+  (( minutes > 0 )) && human+="${minutes}m "
+  human+="${seconds}s"
+
+  geometry_time_human=$human
+}
+
+-prompt_geometry_time_short_format() {
+  local human=""
+  local days=$1
+  local hours=$2
+  local minutes=$3
+  local seconds=$4
+
+  if (( days > 0 )); then
+    human="${days}d"
+  elif (( hours > 0 )); then
+    human="${hours}h"
+  elif (( minutes > 0 )); then
+    human="${minutes}m"
+  else
+    human="${seconds}s"
+  fi
+
+  geometry_time_human=$human
+}
+
+prompt_geometry_seconds_to_human_time() {
+  local total_seconds=$(prompt_geometry_git_time_since_commit)
+  # local total_seconds=$1
+
+  local days=$(( total_seconds / 60 / 60 / 24 ))
+  local hours=$(( total_seconds / 60 / 60 % 24 ))
+  local minutes=$(( total_seconds / 60 % 60 ))
+  local seconds=$(( total_seconds % 60 ))
+
+  # It looks redundant but it seems it's not
+  -prompt_geometry_time_short_format $days $hours $minutes $seconds
+
+  echo $geometry_time_human
+}
+
+prompt_geometry_git_time_since_commit() {
+  # Defaults to "", which would hide the git_time_since_commit block
+  local git_time_since_commit=""
+
+  # Get the last commit.
+  local last_commit=$(git log -1 --pretty=format:'%at' 2> /dev/null)
+  if [[ -n $last_commit ]]; then
+      now=$(date +%s)
+      seconds_since_last_commit=$((now - last_commit))
+      git_time_since_commit=$seconds_since_last_commit
+  fi
+
+  echo $git_time_since_commit
+}
