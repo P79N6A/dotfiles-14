c34cf328596c7532c7c84a03d94ad1e8b5171a54
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-02-02 23:08:22 +0700

    Update source

--- zshrc.sh
+++ zshrc.sh
@@ -49,7 +49,7 @@ DISABLE_AUTO_UPDATE="true"
 # DISABLE_LS_COLORS="true"
 
 # Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
+DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
 # ENABLE_CORRECTION="true"
@@ -78,7 +78,7 @@ DISABLE_UNTRACKED_FILES_DIRTY="true"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git gitfast time zsh-autosuggestions zsh-completions zsh-256color)
+plugins=(git gitfast time zsh-autosuggestions zsh-completions)
 
 # source $ZSH/oh-my-zsh.sh
 
