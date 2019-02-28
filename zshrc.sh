f81d5f61fca8318b5011f411992347aed62b70b9
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-02-28 16:27:51 +0700

    Update source

--- zshrc.sh
+++ zshrc.sh
@@ -78,7 +78,7 @@ HIST_STAMPS="yyyy-mm-dd"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=('git' 'gitfast' 'time' 'zsh-autosuggestions' 'zsh-completions')
+plugins=(git gitfast time zsh-autosuggestions zsh-completions)
 
 # source $ZSH/oh-my-zsh.sh
 
