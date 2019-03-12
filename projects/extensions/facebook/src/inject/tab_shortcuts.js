dd6236ae6c35fad03af59bb69478da14555e632a
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-03-13 02:56:18 +0700

    Update source

--- projects/extensions/facebook/src/inject/tab_shortcuts.js
+++ projects/extensions/facebook/src/inject/tab_shortcuts.js
@@ -6,9 +6,51 @@
 * Register a callback function with the commands api, which will be called when
 * one of our registered commands is detected.
 */
+
+let hasPinTabs = false;
+let windowTabs = null;
+let firstTab = null;
+let lastTab = null;
+let firstPinTab = null;
+let lastPinTab = null;
+let firstIndex = 0;
+let lastIndex = -1;
+let firstPinIndex = 0;
+let lastPinIndex = -1;
+
 chrome.commands.onCommand.addListener(function(command) {
   // Call 'update' with an empty properties object to get access to the current
   // tab (given to us in the callback function).
+  chrome.tabs.query({
+    'currentWindow': true,
+    'pinned': false,
+  }, function (tabs) {
+    windowTabs = tabs;
+    if (windowTabs.length > 0) {
+      firstTab = windowTabs[0];
+      lastTab = windowTabs[(windowTabs.length - 1)];
+    }
+  });
+
+  chrome.tabs.query({
+    'currentWindow': true,
+    'pinned': true,
+  }, function (tabs) {
+    windowPinTabs = tabs;
+    if (windowPinTabs.length > 0) {
+      hasPinTabs = true;
+      firstPinTab = windowPinTabs[0];
+      lastPinTab = windowPinTabs[(windowPinTabs.length - 1)];
+    }
+  });
+
+  if (hasPinTabs == true) {
+    firstIndex = firstTab.index;
+    lastIndex = lastTab.index;
+    firstPinIndex = firstPinTab.index;
+    lastPinIndex = lastPinTab.index;
+  }
+
   chrome.tabs.query({
     'currentWindow': true,
     'active': true,
@@ -18,13 +60,37 @@ chrome.commands.onCommand.addListener(function(command) {
     if (command == 'duplicate-tab') {
       chrome.tabs.duplicate(activeTab.id);
     } else if (command == 'move-tab-left') {
+      let newIndex = parseInt(activeTab.index) - 1;
+
+      if (activeTab.pinned == true) {
+        if (newIndex < firstPinIndex) {
+          newIndex = lastPinIndex;
+        }
+      } else {
+        if (newIndex < firstIndex) {
+          newIndex = lastIndex;
+        }
+      }
+
       chrome.tabs.move(activeTab.id, {
-        'index': parseInt(activeTab.index) - 1,
+        'index': newIndex,
         'windowId': parseInt(activeTab.windowId),
       });
     } else if (command == 'move-tab-right') {
+      let newIndex = parseInt(activeTab.index) + 1;
+
+      if (activeTab.pinned == true) {
+        if (newIndex > lastPinIndex) {
+          newIndex = firstPinIndex;
+        }
+      } else {
+        if (newIndex > lastIndex) {
+          newIndex = firstIndex;
+        }
+      }
+
       chrome.tabs.move(activeTab.id, {
-        'index': parseInt(activeTab.index) + 1,
+        'index': newIndex,
         'windowId': parseInt(activeTab.windowId),
       });
     }
