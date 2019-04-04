005aa125a0d68e64be36d17a4104384feed7e9cf
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-04-04 22:47:16 +0700

    Update source

--- zshrc.sh
+++ zshrc.sh
@@ -25,6 +25,14 @@ export ZSH=$ZSHFILES
 # ZSH_THEME="kolo"
 # ZSH_THEME="nodeys"
 ZSH_THEME="nanotech"
+SPACESHIP_PROMPT_ADD_NEWLINE="true"
+SPACESHIP_CHAR_SYMBOL=" \uf0e7"
+SPACESHIP_CHAR_PREFIX="\uf296"
+SPACESHIP_CHAR_SUFFIX=(" ")
+SPACESHIP_CHAR_COLOR_SUCCESS="yellow"
+SPACESHIP_PROMPT_DEFAULT_PREFIX="$USER"
+SPACESHIP_PROMPT_FIRST_PREFIX_SHOW="true"
+SPACESHIP_USER_SHOW="true"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
