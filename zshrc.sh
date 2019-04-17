445b5d43db55a40cdfcce7af431b9d335247fc4b
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-04-17 22:04:00 +0700

    Update source

--- zshrc.sh
+++ zshrc.sh
@@ -22,10 +22,11 @@ export ZSH=$ZSHFILES
 # ZSH_THEME="zhann"
 # ZSH_THEME="ys"
 # ZSH_THEME="smt"
-ZSH_THEME="kolo"
+# ZSH_THEME="kolo"
 # ZSH_THEME="nodeys"
 # ZSH_THEME="nanotech"
 # ZSH_THEME="bureau"
+ZSH_THEME="ys"
 
 SPACESHIP_PROMPT_ADD_NEWLINE="true"
 SPACESHIP_CHAR_SYMBOL=" \uf0e7"
