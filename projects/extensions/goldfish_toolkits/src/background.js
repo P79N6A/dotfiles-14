4f1a2f712b2c8e2cb03870be780a395d25657e0a
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-05-12 19:17:16 +0700

    Update source

--- projects/extensions/goldfish_toolkits/src/background.js
+++ projects/extensions/goldfish_toolkits/src/background.js
@@ -33,13 +33,13 @@ chrome.runtime.onInstalled.addListener(function () {
   //     'link',
   //   ],
   // });
-  // var SaveImageToDownload = chrome.contextMenus.create({
-  //   'id': 'SaveImageToDownload',
-  //   'title': 'Save image to download',
-  //   'contexts': [
-  //     'image',
-  //   ],
-  // });
+  var SaveImageToDownload = chrome.contextMenus.create({
+    'id': 'SaveImageToDownload',
+    'title': 'Save Image',
+    'contexts': [
+      'image',
+    ],
+  });
   var OpenFrameInTab = chrome.contextMenus.create({
     'id': 'OpenFrameInTab',
     'title': 'Open Frame in tab',
@@ -124,7 +124,13 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
       });
     }
   } else if (info.menuItemId == 'SaveImageToDownload') {
-    alert(window.location.href);
+    if (info.mediaType == 'image') {
+      if (info.srcUrl) {
+        chrome.downloads.download({
+          'url': info.srcUrl,
+        });
+      }
+    }
   } else if (info.menuItemId == 'OpenGoogleTranslate') {
     var selectionText = '';
     if (typeof info.selectionText != 'undefined') {
