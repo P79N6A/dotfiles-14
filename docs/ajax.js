ac283211e0823fe532a31c240f41c1d7c7236fa6
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-05-27 14:31:27 +0700

    Update source

--- docs/ajax.js
+++ docs/ajax.js
@@ -1,15 +1,19 @@
 // set endpoint and your access key
-var ip = '134.201.250.155'
-var access_key = 'YOUR_ACCESS_KEY';
+// var ip = '134.201.250.155';
+// var access_key = '3e838d405e314c1e648cb91bed732780';
+var queryString = {
+  url: 'http://api.ipapi.com/api/{{ip}}',
+  ip: '134.201.250.155',
+  access_key: '3e838d405e314c1e648cb91bed732780',
+  hostname: '1',
+  security: '1',
+  output: 'json',
+};
 
 // get the API result via jQuery.ajax
 $.ajax({
-    url: 'http://api.ipapi.com/api/134.201.250.155?access_key=3e838d405e314c1e648cb91bed732780&hostname=1&security=1&output=xml&fields=location',
-    dataType: 'jsonp',
-    success: function(json) {
-
-        // output the "calling_code" object inside "location"
-        alert(json.location.calling_code);
-
-    }
+  url: 'http://api.ipapi.com/api/'+ ip +'?access_key='+ access_key +'&hostname='+ hostname +'&security='+ security +'&output='+ output +'',
+  success: function(response) {
+    alert(response.location.calling_code);
+  },
 });
