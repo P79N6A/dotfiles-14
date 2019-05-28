e16fbd65c81abd8020107fcd9cf60f9d09addc72
nopphasinara <nopphasin.arayasirikul@gmail.com>
2019-05-28 18:05:41 +0700

    Update source

--- docs/ajax.js
+++ docs/ajax.js
@@ -17,3 +17,38 @@ $.ajax({
     alert(response.location.calling_code);
   },
 });
+
+
+
+var ip = '134.201.250.155';
+var url = 'http://api.ipapi.com/api/'+ ip +'';
+var query = '';
+var params = {
+  access_key: '3e838d405e314c1e648cb91bed732780',
+  hostname: '',
+  security: '',
+  output: 'json',
+};
+var data = [];
+
+Object.keys(params).map(function (key, index) {
+  var value = params[key];
+  data.push(([key, encodeURI(value)]).join('='));
+});
+
+if (typeof data !== 'undefined' && data && data.length > 0) {
+  data = data.join('&');
+  query = '?'+ data +'';
+}
+
+console.log(url + query);
+
+window.open(url + query, '_top');
+
+// $.ajax({
+//   url: ''+ url + query +'',
+//   dataType: ''+ params.output +'',
+//   success: function(response) {
+//     console.log(JSON.stringify(response));
+//   },
+// });
